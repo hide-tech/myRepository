04567c213edf84b152c0567b04beffa0c8844f28
hide-tech <hidetechyy@gmail.com>
2022-04-07 14:32:42 +0300

    Add project's structure

---
+++ src/main/java/com/yazykov/projectf/ProjectFirstApplication.java
@@ -0,0 +1,13 @@
+package com.yazykov.projectf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFirstApplication.class, args);
+	}
+
+}
